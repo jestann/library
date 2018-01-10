78f028c93cfe57a9ce3a4f01030f26fe230056b2
Jess Bird <jessannbird@gmail.com>
2018-01-10 08:17:18 +0000

    configuring webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,61 @@
 const path = require('path')
 const webpack = require('webpack')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-const ENV = process.env.NODE_ENV
+// const ENV = process.env.NODE_ENV
 
 const baseConfig = {
   context: path.resolve(__dirname, 'app'),
   entry: {
     main: './index.js',
+    vendor: ['react', 'react-dom'] /* separately bundles vendor code */
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].bundle.js',
+    filename: '[name].bundle.js', /* load script from /build/main.bundle.js */
+    /* publicPath: '/build' */
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'] /* order in which to look; for instance, our directory then node_modules */
   },
   devServer: {
-    contentBase: './src',
-    publicPath: '/output'
+    contentBase: path.resolve(__dirname, 'app'),
+    publicPath: '/build'
   },
+  
+  test: /\.js$/i,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['env'] },
+        }],
+  
   module: {
     rules: [
       {
-        test: /\.scss/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        test: /\.scss/, /* /\.(sass|scss)$/i, */
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ] /* load in reverse array order */
       },
       {
         test: /\.js$/,
-        use: { 
+        exclude: [/node_modules/],
+        use: [{ 
           loader: 'babel-loader',
           options: { presets: ['react', 'es2016'] }
-        }
+        }]
       }
     ]
   },
   plugins: [
-    new HTMLWebpackPlugin(),
-    /* specifies environment */
-    new webpack.DefinePlugin({ 
+    new HTMLWebpackPlugin({title: 'library'}), /* loads multiple scripts in auto-generated build/index.html */
+    /* new webpack.DefinePlugin({ 
       'process.env.NODE_ENV': JSON.stringify(ENV)
-    })
+    }), */
+    new webpack.optimize.CommonsChunkPlugin({ /* separately bundles vendor code */
+      name: 'vendor',
+      minChunks: Infinity,
+    })  
   ]
 }
 
-if (ENV === 'production') {
-  baseConfig.plugins.push(new webpack.optimize.UglifyJsPlugin())
-}
-
+// if (ENV === 'production') { baseConfig.plugins.push(new webpack.optimize.UglifyJsPlugin()) }
 module.exports = baseConfig
-
-
-
-
-/*
-output: {
-    filename: '[name].js',
-    path: path.resolve('./build')
-  }
-*/
