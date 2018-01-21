b30e25a8ab57c6dffc49ee4c8572e68b3bfcd14b
Jess Bird <jessannbird@gmail.com>
2018-01-21 17:48:01 +0000

    scaffolding with redux

--- app/index.js
+++ app/index.js
@@ -1,31 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+
+// redux
+import reducer from './store/reducers'
+let store = createStore(reducer)
+
+// styles
 // import 'bootstrap/dist/css/bootstrap.css';
 // import registerServiceWorker from './tools/registerServiceWorker'
 
+// react
 import 'main.css'
 import App from './components/app'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-// registerServiceWorker()
-
-/* add redux
-
-import { createStore } from 'redux';
-import myApp from './reducers';
-let store = createStore(myApp);
-function render() {
+function render () {
   ReactDOM.render(
-    <div className="container">
-      <App store={store}/>
-      <hr />
-      <Results store={store}/>
+    <div className="index">
+      <App store={store}/> 
     </div>,
     document.getElementById('root')
-  );
+  )
 }
-store.subscribe(render);
-render();
 
-*/
+store.subscribe(render)
+render()
+// registerServiceWorker()
+
+// can add additional components in div with same store, like <Results store={store}/>
